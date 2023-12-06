50790e46b283a51936be3d3099765a7b84ba361c
Sehan Madushanka <43729924+sehanmadu95@users.noreply.github.com>
2023-12-06 21:28:52 +0530

    decision(if else if else) and operators in java

---
+++ Java Full/src/DecisionMaking.java
@@ -0,0 +1,44 @@
+public class  DecisionMaking {
+    public static void main(String[] args) {
+
+        //arithmatic operator
+
+        //addition and concatination
+        System.out.println(4+5);
+        System.out.println("shehan" +" madushanka");
+
+
+        // IF ELSE
+
+        int x=5;
+        if (x==5){
+            System.out.println("Correct");
+            System.out.println("correct2");}
+        else {
+            System.out.println("Wrong");
+        }
+
+        //if else if else
+
+
+        if (5>10)
+        System.out.println("chek");
+        System.out.println("chek2");
+
+    // AND OR NOT && || !
+
+
+
+         // binary operator --> two operends eg : x=5
+        //Unary operator  --> !(5>3) --> (5>3) is complete operation
+
+        //Ternary operator  --> x=10  , x=(5>4)true?false:
+
+        boolean b=(10>5)?true:false;
+        System.out.println(b);
+
+        //Assigment operator
+        int ass=10;
+        ass+=1 ; //ass=ass+1
+    }
+}
