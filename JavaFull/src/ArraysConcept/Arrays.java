c431c6ad2bbc345de00695285c067cdc3fcb6c9d
Sehan Madushanka <43729924+sehanmadu95@users.noreply.github.com>
2023-12-26 10:01:16 +0530

    arrays java

---
+++ JavaFull/src/ArraysConcept/Arrays.java
@@ -0,0 +1,32 @@
+package ArraysConcept;
+
+public class Arrays {
+    public static void main(String[] args) {
+         int [] numbers={1,2,5,9,7};
+
+         //declration
+         String [] name;
+
+         //initilization
+         name =new String[5];
+         name[0]="shehan";
+
+
+         for(int x=0;x<numbers.length;x++){
+             System.out.println(numbers[x]);
+         }
+
+         for(int x:numbers){
+             System.out.println(x);
+         }
+
+    }
+}
+
+
+/**
+*issues with arrays
+
+The size of the array cannot be
+increased or decreased once it is declared—arrays have a fixed size.*/
+
